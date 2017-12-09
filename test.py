f2bee016499241baab33dcfc2f6b46e046386f6c
Leon <Strikerklm96@gmail.com>
2017-12-09 12:52:35 -0600

    Save Graph and Brain;

--- test.py
+++ test.py
@@ -7,13 +7,13 @@ inputType = tf.placeholder(shape=[None, num_inputs], dtype=tf.float32)
 outputType = tf.placeholder(shape=[None, num_labels], dtype=tf.float32)
 
 def buildNetwork(ins):
-    network = tf.layers.dense(name="leon", inputs=ins, units=num_labels, activation=tf.nn.sigmoid)
+    network = tf.layers.dense(name="leon", inputs=ins, units=num_labels)
     return network
 
 
 network = buildNetwork(inputType)
 loss = tf.square(network - outputType)
-vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "leon/kernel:0")
+vars = tf.get_collection(key=tf.GraphKeys.TRAINABLE_VARIABLES, scope="leon/bias:0") #  "leon/bias:0"
 train_op = tf.train.GradientDescentOptimizer(0.1).minimize(loss=loss, var_list=vars)
 
 init_op = tf.global_variables_initializer()
@@ -22,11 +22,16 @@ sess.run(init_op)
 
 inputs = [[0], [1]]
 outputs = [[0], [2]]
+
+brainSaver = tf.train.Saver()
+graphWriter = tf.summary.FileWriter("output/", sess.graph)
+
 for i in range(500):
     print(sess.run(network, feed_dict={inputType: inputs}))
     sess.run(fetches=[train_op, loss], feed_dict={inputType: inputs, outputType: outputs})
 
-
+brainSaver.save(sess, "brains/")
+graphWriter.close()
 
 
 
