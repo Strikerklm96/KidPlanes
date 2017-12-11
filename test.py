01f882ac22bd78a94bc54f38c16dac96a2ceccf4
Leon <Strikerklm96@gmail.com>
2017-12-10 21:54:14 -0600

    Easy Network;

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 import tensorflow as tf
 
-num_inputs = 1
-num_labels = 1
+#
+tf.FLAGS.num_inputs = 1
+tf.FLAGS.num_labels = 1
 
-inputType = tf.placeholder(shape=[None, num_inputs], dtype=tf.float32)
-outputType = tf.placeholder(shape=[None, num_labels], dtype=tf.float32)
+inputType = tf.placeholder(shape=[None, tf.FLAGS.num_inputs], dtype=tf.float32)
+outputType = tf.placeholder(shape=[None, tf.FLAGS.num_labels], dtype=tf.float32)
 
 def buildNetwork(ins):
-    network = tf.layers.dense(name="leon1", inputs=ins, units=num_labels)
-    network2 = tf.layers.dense(name="leon2", inputs=network, units=num_labels)
-    return network2
+    network = tf.layers.dense(name="leon1", inputs=ins, units=tf.FLAGS.num_labels)
+    #network = tf.layers.dense(name="leon2", inputs=network, units=num_labels)
+    return network
+
+
 
 
 network = buildNetwork(inputType)
@@ -22,28 +25,34 @@ init_op = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init_op)
 
-inputs = [[0], [1]]
-outputs = [[0], [2]]
+#Set the expected inputs and outputs.
+inputs = [[0], [1]] #x values
+outputs = [[1], [3]] #expected y values
 
+#Setup the saver and graph writer.
 brainSaver = tf.train.Saver()
 graphWriter = tf.summary.FileWriter("output/", sess.graph)
 
-
-print("\n Initial Network:")
+#Print the initial values of the network.
+print("\n Initial Network [weight, bias]:")
 print(sess.run(vars1))
 print(sess.run(vars2))
 
+#Train the network for N training events.
 for i in range(100):
- #   print(sess.run(network, feed_dict={inputType: inputs}))
+    #print(sess.run(network, feed_dict={inputType: inputs})) #print the results of output run.
     sess.run(fetches=[train_op, loss], feed_dict={inputType: inputs, outputType: outputs})
 
+#Print a single sample output.
 print("\n Final Outputs:")
 print(sess.run(network, feed_dict={inputType: inputs}))
 
-print("\n Final Network:")
+#Print results of network.
+print("\n Final Network [weight, bias]:")
 print(sess.run(vars1))
 print(sess.run(vars2))
 
+#Save the trained network values and write to the tensorboard.
 brainSaver.save(sess, "brains/")
 graphWriter.close()
 
