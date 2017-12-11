7f78cad459683ed1df2f3d6e1b4e2f54c91fcc9b
Leon <Strikerklm96@gmail.com>
2017-12-11 17:40:58 -0600

    Base Comments;

--- test.py
+++ test.py
@@ -7,9 +7,10 @@ tf.FLAGS.num_labels = 1
 inputType = tf.placeholder(shape=[None, tf.FLAGS.num_inputs], dtype=tf.float32)
 outputType = tf.placeholder(shape=[None, tf.FLAGS.num_labels], dtype=tf.float32)
 
+
 def buildNetwork(ins):
     network = tf.layers.dense(name="leon1", inputs=ins, units=tf.FLAGS.num_labels)
-    #network = tf.layers.dense(name="leon2", inputs=network, units=num_labels)
+    # network = tf.layers.dense(name="leon2", inputs=network, units=num_labels)
     return network
 
 
@@ -25,34 +26,34 @@ init_op = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init_op)
 
-#Set the expected inputs and outputs.
+# Set the expected inputs and outputs.
 inputs = [[0], [1]] #x values
 outputs = [[1], [3]] #expected y values
 
-#Setup the saver and graph writer.
+# Setup the saver and graph writer.
 brainSaver = tf.train.Saver()
 graphWriter = tf.summary.FileWriter("output/", sess.graph)
 
-#Print the initial values of the network.
+# Print the initial values of the network.
 print("\n Initial Network [weight, bias]:")
 print(sess.run(vars1))
 print(sess.run(vars2))
 
-#Train the network for N training events.
+# Train the network for N training events.
 for i in range(100):
-    #print(sess.run(network, feed_dict={inputType: inputs})) #print the results of output run.
+    # print(sess.run(network, feed_dict={inputType: inputs})) #print the results of output run.
     sess.run(fetches=[train_op, loss], feed_dict={inputType: inputs, outputType: outputs})
 
-#Print a single sample output.
+# Print a single sample output.
 print("\n Final Outputs:")
 print(sess.run(network, feed_dict={inputType: inputs}))
 
-#Print results of network.
+# Print results of network.
 print("\n Final Network [weight, bias]:")
 print(sess.run(vars1))
 print(sess.run(vars2))
 
-#Save the trained network values and write to the tensorboard.
+# Save the trained network values and write to the tensorboard.
 brainSaver.save(sess, "brains/")
 graphWriter.close()
 
