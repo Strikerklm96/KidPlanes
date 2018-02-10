cd619c54d3ff8974b8c410639f433d2578b7aa2d
Leon <Strikerklm96@gmail.com>
2018-02-09 21:27:18 -0600

    Minor Comment;

--- echo_0.py
+++ echo_0.py
@@ -81,7 +81,7 @@ for current_input in inputs_series:
 # LSTM: the states_series is storing the top line having already been tanh, multiplying x and adding bias +
 # LSTM: logits_series is the LSTM output ht, computed by tanh(state) * w2 + b
 logits_series = [tf.matmul(state, W2) + b2 for state in states_series]
-# apply softmax (which basically just turns our guesses into more appropriate probability guess, (0.1, 0.4)->(0.2, 0.8) horray 10 is not a prime
+# apply softmax (which basically just turns our guesses into more appropriate probability guess, (0.1, 0.4)->(0.2, 0.8) horray 10 (our base of numbers) is not a prime
 predictions_series = [tf.nn.softmax(logits) for logits in logits_series]
 
 losses = [tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels) for logits, labels in
